e91a37a31dd01ee053f3bd7aa297a5199515e48a
Kathryn Teall <katgrace0808@gmail.com>
2020-06-21 09:12:34 -0700

    Initial commit with mysql files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('California Burger');
+INSERT INTO burgers (burger_name) VALUES ('Wild Salmon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Portobello Mushroom Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheeseburger');
